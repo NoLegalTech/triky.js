1c70ed991d5f55b7750100de2d9054fb099c0058
Pepe Doval <pepellou@gmail.com>
2018-03-09 19:48:53 +0100

    Accept url as parameter
    This way the gulp task can still be used without prompt, useful for
    scripting.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
 var exec = require('child_process').exec;
 var readlineSync = require('readline-sync');
+var argv = require('yargs').argv;
 
 gulp.task('default', function(cb) {
-    var url = readlineSync.question('Enter url: ');
+    var url = argv.url;
+    if (url === undefined) {
+        url = readlineSync.question('Enter url: ');
+    }
     exec('vendor/phantomjs grab_cookies.js ' + url, function (err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
