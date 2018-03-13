82d288451d79a6d91679ba61acefde3821a2e914
Pepe Doval <pepellou@gmail.com>
2018-03-13 09:25:42 +0100

    Listen on port 8080 (for OpenShift)
    This is done because the script does not seem to get the env variable
    for the port.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var triky = require('./'),
     express = require('express'),
     app = express(),
-    port = process.env.OPENSHIFT_NODEJS_PORT || 3000,
+    port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
     server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 
