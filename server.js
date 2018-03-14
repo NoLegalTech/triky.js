68166574b9d9380dadbc8e6a313881607b0224ca
Pepe Doval <pepellou@gmail.com>
2018-03-14 12:36:27 +0100

    Adapt for Heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var triky = require('./'),
     cors = require('cors'),
     express = require('express'),
     app = express(),
-    port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.use(cors());
