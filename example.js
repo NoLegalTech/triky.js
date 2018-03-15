62493fd02d4a5b27c5654958f41cb76aac02d2b5
Pepe Doval <pepellou@gmail.com>
2018-03-15 13:02:06 +0100

    Add command arguments

--- example.js
+++ example.js
@@ -4,12 +4,19 @@
 require('console.table');
 
 var triky = require('./'),
-    url = 'http://nolegaltech.com';
+    optionDefinitions = [
+        { name: 'policy', alias: 'p', type: Boolean },
+        { name: 'template', alias: 't', type: String, defaultValue: 'templates/default.html' },
+        { name: 'url', alias: 'u', type: String, defaultOption: true, defaultValue: 'http://nolegaltech.com' }
+    ],
+    commandLineArgs = require('command-line-args'),
+    options = commandLineArgs(optionDefinitions);
 
-if (process.argv.length > 2) {
-    url = process.argv[2];
-}
-
-triky.grab(url, function (cookies) {
-    console.table(cookies);
+triky.grab(options.url, function (cookies) {
+    if (options.policy) {
+        console.log('Generating policy for ' + options.url + '...');
+        console.log('Using template at ' + options.template);
+    } else {
+        console.table(cookies);
+    }
 });
