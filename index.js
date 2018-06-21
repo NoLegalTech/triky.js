aa4a0d227d5f9d44fc63ff16668077aaf39cbf83
Pepe Doval <pepellou@gmail.com>
2018-06-21 22:50:59 +0200

    Calculate expiration time for cookie

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var moment = require('moment');
+
 var phantom = require('phantom'),
     _ph,
     _page,
@@ -51,7 +53,60 @@ module.exports.grab = function triky(url, callback) {
             })
             .then(function(cookies) {
                 _ph.exit();
-                resolve(cookies);
+                var result = [];
+                var now = moment().format('X');
+                cookies.forEach(function(cookie) {
+                    var secs = cookie.expiry - now + 1;
+                    var mins = 0;
+                    var hours = 0;
+                    var days = 0;
+                    var years = 0;
+                    if (secs >= 60) {
+                        mins = Math.floor(secs / 60);
+                        secs = secs % 60;
+                        if (mins >= 60) {
+                            hours = Math.floor(mins / 60);
+                            mins = mins % 60;
+                            if (hours >= 24) {
+                                days = Math.floor(hours / 24);
+                                hours = hours % 24;
+                                if (days >= 365) {
+                                    years = Math.floor(days / 365);
+                                    days = days % 365;
+                                }
+                            }
+                        }
+                    }
+                    cookie.expirationTime = '';
+                    if (years > 1) {
+                        cookie.expirationTime = cookie.expirationTime + years + ' years ';
+                    } else if (years == 1) {
+                        cookie.expirationTime = cookie.expirationTime + ' 1 year ';
+                    }
+                    if (days > 1) {
+                        cookie.expirationTime = cookie.expirationTime + days + ' days ';
+                    } else if (days == 1) {
+                        cookie.expirationTime = cookie.expirationTime + ' 1 day ';
+                    }
+                    if (hours > 1) {
+                        cookie.expirationTime = cookie.expirationTime + hours + ' h ';
+                    } else if (hours == 1) {
+                        cookie.expirationTime = cookie.expirationTime + ' 1 h ';
+                    }
+                    if (mins > 1) {
+                        cookie.expirationTime = cookie.expirationTime + mins + ' min ';
+                    } else if (mins == 1) {
+                        cookie.expirationTime = cookie.expirationTime + ' 1 min ';
+                    }
+                    if (secs > 1) {
+                        cookie.expirationTime = cookie.expirationTime + secs + ' s ';
+                    } else if (secs == 1) {
+                        cookie.expirationTime = cookie.expirationTime + ' 1 s ';
+                    }
+
+                    result.push(cookie);
+                });
+                resolve(result);
                 return callback(cookies);
             })
             .catch(function(err) {
