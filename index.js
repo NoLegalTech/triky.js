536351411f91d047e906cbf21073ccca7c5f0e22
Pepe Doval <pepellou@gmail.com>
2018-06-27 18:39:43 +0200

    Refactor: simplify expirationTime algorithm

--- index.js
+++ index.js
@@ -15,52 +15,32 @@ var Triky = function() {
     self.getExpirationTime = function(cookie) {
         var now = moment().format('X');
         var secs = cookie.expiry - now + 1;
-        var mins = 0;
-        var hours = 0;
-        var days = 0;
-        var years = 0;
-        if (secs >= 60) {
-            mins = Math.floor(secs / 60);
-            secs = secs % 60;
-            if (mins >= 60) {
-                hours = Math.floor(mins / 60);
-                mins = mins % 60;
-                if (hours >= 24) {
-                    days = Math.floor(hours / 24);
-                    hours = hours % 24;
-                    if (days >= 365) {
-                        years = Math.floor(days / 365);
-                        days = days % 365;
-                    }
-                }
-            }
-        }
+
+        var time_units = [
+            { unit: 's',    plural: 's',     value: 1 },
+            { unit: 'min',  plural: 'min',   value: 60 },
+            { unit: 'h',    plural: 'h',     value: 60 * 60 },
+            { unit: 'day',  plural: 'days',  value: 60 * 60 * 24 },
+            { unit: 'year', plural: 'years', value: 60 * 60 * 24 * 365 }
+        ];
+
+        var time_unit = time_units.pop();
+
         cookie.expirationTime = '';
-        if (years > 1) {
-            cookie.expirationTime = cookie.expirationTime + ' ' + years + ' years ';
-        } else if (years == 1) {
-            cookie.expirationTime = cookie.expirationTime + ' 1 year ';
+
+        while (secs > 0) {
+            var num_units = Math.floor(secs / time_unit.value);
+            if (num_units > 0) {
+                cookie.expirationTime += ' '
+                    + num_units
+                    + ' '
+                    + (num_units == 1 ? time_unit.unit : time_unit.plural)
+                    + ' ';
+                secs -= num_units * time_unit.value;
+            }
+            time_unit = time_units.pop();
         }
-        if (days > 1) {
-            cookie.expirationTime = cookie.expirationTime + ' ' + days + ' days ';
-        } else if (days == 1) {
-            cookie.expirationTime = cookie.expirationTime + ' 1 day ';
-        }
-        if (hours > 1) {
-            cookie.expirationTime = cookie.expirationTime + ' ' + hours + ' h ';
-        } else if (hours == 1) {
-            cookie.expirationTime = cookie.expirationTime + ' 1 h ';
-        }
-        if (mins > 1) {
-            cookie.expirationTime = cookie.expirationTime + ' ' + mins + ' min ';
-        } else if (mins == 1) {
-            cookie.expirationTime = cookie.expirationTime + ' 1 min ';
-        }
-        if (secs > 1) {
-            cookie.expirationTime = cookie.expirationTime + ' ' + secs + ' s ';
-        } else if (secs == 1) {
-            cookie.expirationTime = cookie.expirationTime + ' 1 s ';
-        }
+
         return cookie;
     };
 
