ded76fb62a4521e0ffc4a46e58c4a736b5b59ad2
Pepe Doval <pepellou@gmail.com>
2018-06-29 19:11:12 +0200

    Get cookies info from WTC

--- example.js
+++ example.js
@@ -4,6 +4,8 @@
 require('console.table');
 
 var triky = require('./'),
+    request = require('request'),
+    cTable = require('console.table'),
     optionDefinitions = [
         { name: 'policy', alias: 'p', type: Boolean },
         { name: 'template', alias: 't', type: String, defaultValue: 'templates/default.html' },
@@ -12,11 +14,56 @@ var triky = require('./'),
     commandLineArgs = require('command-line-args'),
     options = commandLineArgs(optionDefinitions);
 
+var printCookie = function(cookie) {
+    var table = [];
+    table.push({ key: 'name', value: cookie.name });
+    table.push({ key: 'domain', value: cookie.domain });
+    table.push({ key: 'expires', value: cookie.expires });
+    table.push({ key: 'expiry', value: cookie.expiry });
+    table.push({ key: 'httponly', value: cookie.httponly });
+    table.push({ key: 'path', value: cookie.path });
+    table.push({ key: 'secure', value: cookie.secure });
+    table.push({ key: 'value', value: cookie.value });
+    table.push({ key: 'expirationTime', value: cookie.expirationTime });
+    if (cookie.additionalInfo) {
+        table.push({ key: 'WTC.wtc_id', value: cookie.additionalInfo.wtc_id });
+        table.push({ key: 'WTC.name', value: cookie.additionalInfo.name });
+        table.push({ key: 'WTC.origin', value: cookie.additionalInfo.origin });
+        table.push({ key: 'WTC.type', value: cookie.additionalInfo.type });
+        table.push({ key: 'WTC.goal', value: cookie.additionalInfo.goal });
+        table.push({ key: 'WTC.expiration_time', value: cookie.additionalInfo.expiration_time });
+        table.push({ key: 'WTC.published', value: cookie.additionalInfo.published });
+        table.push({ key: 'WTC.usage_url', value: cookie.additionalInfo.usage_url });
+        table.push({ key: 'WTC.optout_url', value: cookie.additionalInfo.optout_url });
+        table.push({ key: 'WTC.verified', value: cookie.additionalInfo.verified });
+    }
+    console.table(table);
+
+        /*
+  additionalInfo:
+   { wtc_id: 2,
+        */
+};
+
 triky.grab(options.url, function (cookies) {
     if (options.policy) {
         console.log('Generating policy for ' + options.url + '...');
         console.log('Using template at ' + options.template);
     } else {
-        console.table(cookies);
+        //console.table(cookies);
+        cookies.forEach(function(cookie) {
+            request('https://www.nolegaltech.com/es-ES/wtc/a/' + cookie.name, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    var info = JSON.parse(body)
+                    info.forEach(function(data) {
+                        if (data.name == cookie.name) {
+                            cookie.additionalInfo = data;
+                        }
+                    });
+                    printCookie(cookie);
+                }
+            })
+
+        });
     }
 });
