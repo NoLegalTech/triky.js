9f64c339a99f7be862c2a4b2b1e97f9704d2b617
Pepe Doval <pepellou@gmail.com>
2018-07-05 18:18:17 +0200

    Fix jslint errors in tests

--- tests/triky-test.js
+++ tests/triky-test.js
@@ -1,77 +1,83 @@
+/*jslint node: true */
+/*global
+it, describe
+*/
+"use strict";
+
 var chai = require('chai');
 var expect = chai.expect;
 var triky = require('./../index.js');
 
 var now = require('moment')().format('X');
 
-var addSeconds = function(moment, seconds) {
+var addSeconds = function (moment, seconds) {
     return parseInt(moment) + seconds;
 };
 
-describe('triky', function() {
+describe('triky', function () {
 
-  describe('getExpirationTime() should return the expiration time', function() {
+    describe('getExpirationTime() should return the expiration time', function () {
 
-      it('1 s', function() {
-        expect(
-            triky.getExpirationTime({ expiry: now }).expirationTime
-        ).to.equal(' 1 s ');
-      });
+        it('1 s', function () {
+            expect(
+                triky.getExpirationTime({expiry: now}).expirationTime
+            ).to.equal(' 1 s ');
+        });
 
-      it('1 min', function() {
-        expect(
-            triky.getExpirationTime({ expiry: addSeconds(now, 59) }).expirationTime
-        ).to.equal(' 1 min ');
-      });
+        it('1 min', function () {
+            expect(
+                triky.getExpirationTime({expiry: addSeconds(now, 59)}).expirationTime
+            ).to.equal(' 1 min ');
+        });
 
-      it('1 min 1 s', function() {
-        expect(
-            triky.getExpirationTime({ expiry: addSeconds(now, 60) }).expirationTime
-        ).to.equal(' 1 min  1 s ');
-      });
+        it('1 min 1 s', function () {
+            expect(
+                triky.getExpirationTime({expiry: addSeconds(now, 60)}).expirationTime
+            ).to.equal(' 1 min  1 s ');
+        });
 
-      it('1 h', function() {
-        expect(
-            triky.getExpirationTime({ expiry: addSeconds(now, 3599) }).expirationTime
-        ).to.equal(' 1 h ');
-      });
+        it('1 h', function () {
+            expect(
+                triky.getExpirationTime({expiry: addSeconds(now, 3599)}).expirationTime
+            ).to.equal(' 1 h ');
+        });
 
-      it('1 h 1 min 1 s', function() {
-        expect(
-            triky.getExpirationTime({ expiry: addSeconds(now, 3660) }).expirationTime
-        ).to.equal(' 1 h  1 min  1 s ');
-      });
+        it('1 h 1 min 1 s', function () {
+            expect(
+                triky.getExpirationTime({expiry: addSeconds(now, 3660)}).expirationTime
+            ).to.equal(' 1 h  1 min  1 s ');
+        });
 
-      it('1 day', function() {
-        expect(
-            triky.getExpirationTime({ expiry: addSeconds(now, 3600 * 24 - 1) }).expirationTime
-        ).to.equal(' 1 day ');
-      });
+        it('1 day', function () {
+            expect(
+                triky.getExpirationTime({expiry: addSeconds(now, 3600 * 24 - 1)}).expirationTime
+            ).to.equal(' 1 day ');
+        });
 
-      it('45 days', function() {
-        expect(
-            triky.getExpirationTime({ expiry: addSeconds(now, 45 * 3600 * 24 - 1) }).expirationTime
-        ).to.equal(' 45 days ');
-      });
+        it('45 days', function () {
+            expect(
+                triky.getExpirationTime({expiry: addSeconds(now, 45 * 3600 * 24 - 1)}).expirationTime
+            ).to.equal(' 45 days ');
+        });
 
-      it('364 days 23 h 59 min 59 s', function() {
-        expect(
-            triky.getExpirationTime({ expiry: addSeconds(now, 365 * 3600 * 24 - 2) }).expirationTime
-        ).to.equal(' 364 days  23 h  59 min  59 s ');
-      });
+        it('364 days 23 h 59 min 59 s', function () {
+            expect(
+                triky.getExpirationTime({expiry: addSeconds(now, 365 * 3600 * 24 - 2)}).expirationTime
+            ).to.equal(' 364 days  23 h  59 min  59 s ');
+        });
 
-      it('1 year', function() {
-        expect(
-            triky.getExpirationTime({ expiry: addSeconds(now, 365 * 3600 * 24 - 1) }).expirationTime
-        ).to.equal(' 1 year ');
-      });
+        it('1 year', function () {
+            expect(
+                triky.getExpirationTime({expiry: addSeconds(now, 365 * 3600 * 24 - 1)}).expirationTime
+            ).to.equal(' 1 year ');
+        });
 
-      it('2 years', function() {
-        expect(
-            triky.getExpirationTime({ expiry: addSeconds(now, 2 * 365 * 3600 * 24 - 1) }).expirationTime
-        ).to.equal(' 2 years ');
-      });
+        it('2 years', function () {
+            expect(
+                triky.getExpirationTime({expiry: addSeconds(now, 2 * 365 * 3600 * 24 - 1)}).expirationTime
+            ).to.equal(' 2 years ');
+        });
 
-  });
+    });
 
 });
